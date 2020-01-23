c3e210f20f45e5bd10a4a274bec191ae822a379c
Sergio Canto <sergio@bellcantolai.com>
2020-01-23 12:54:06 +0100

    OpenQuizz nouvel ordi

---
+++ OpenQuizz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenQuizz
+//
+//  Created by Sergio Canto  on 22/01/2020.
+//  Copyright © 2020 Sergio Canto . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
